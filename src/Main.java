40cd9a873cb6725b7ddb616b58d31b9074879c43
melavsr <melikeavsr@gmail.com>
2023-10-21 15:08:49 +0300

    çıkarma işlemi ekledim

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       toplama(7,4);
+       cikarma(9,3);
+        }
+        public static void toplama (int sayi1,int sayi2){
+        int toplam = sayi1 + sayi2;
+            System.out.println("Sayıların toplamı : "+toplam);
+        }
+    public static void cikarma (int sayi1,int sayi2){
+        int cikarma = sayi1 - sayi2;
+        System.out.println("Sayıların Farkı : "+cikarma);
+    }
+    }
+// yeni eklemeler yapacağım
+
+
