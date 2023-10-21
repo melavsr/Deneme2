f8f8ac11b8a67e96fa4da261b3d49d9d462edba2
melavsr <melikeavsr@gmail.com>
2023-10-21 14:57:00 +0300

    ilk projemde toplama yaptım

---
+++ HesapMakinasi.java
@@ -0,0 +1,9 @@
+public class HesapMakinasi {
+    public static void main(String[] args) {
+        toplama(7,4);
+        }
+        public static void toplama (int sayi1,int sayi2){
+            int toplam = sayi1 + sayi2;
+            System.out.println("Sayıların toplamı : "+toplam);
+    }
+}
